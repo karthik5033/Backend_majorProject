42c30fb4c6ac0e77c0ae4b7ff13e4812427b76cb
karthik5033 <karthikkp5033@gmail.com>
2025-09-15 02:33:26 +0530

    a

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 let app = express();
-// basic configuration
+// basic configuration.
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
