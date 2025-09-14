0cf07126fccc16f28588c6bc1bc7311f31fb0a6c
karthik5033 <karthikkp5033@gmail.com>
2025-09-14 20:55:41 +0530

    a

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 let app = express();
-app.use(express.json({limit}));
+// basic configuration
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
   res.send("welcome to basecampy");
 });
