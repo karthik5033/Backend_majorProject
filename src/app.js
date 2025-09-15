2837d3c3a63278f97503746eae2ccd2c36f5498c
karthik5033 <karthikkp5033@gmail.com>
2025-09-16 01:46:00 +0530

    a

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 let app = express();
 // basic configuration.
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+// cors configuration
+app.use(cors({
+  origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173"
+}))
 
 app.get("/", (req, res) => {
   res.send("welcome to basecampy");
