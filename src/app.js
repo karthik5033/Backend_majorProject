539941145e3777c8d29710d015e505b45df1bc6a
karthik5033 <karthikkp5033@gmail.com>
2025-09-18 20:49:14 +0530

    a

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ import express from "express";
 import cors from "cors";
 let app = express();
 // basic configuration.
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 // cors configuration
-app.use(cors({
-  origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173"
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+  }),
+);
+
+// import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome to basecampy");
