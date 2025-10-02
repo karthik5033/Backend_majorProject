4a754077bf4e3401e8c57fbf6f69139e65bd6ed5
karthik5033 <karthikkp5033@gmail.com>
2025-10-02 14:54:14 +0530

    added new controllers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+import cookieParser from "cookie-parser";
 
 let app = express();
 // basic configuration.
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 // cors configuration
 app.use(
   cors({
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -70,7 +70,7 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
-userSchema.methods.generateAcessToken = function () {
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
